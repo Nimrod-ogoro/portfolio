3d76588e9acd87115e718a788b3a8b7d4a4cac3b
Nimrod-ogoro <nimrodogoro@gmail.com>
2026-06-03 09:30:53 +0300

    cmc

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,7 @@
 import './App.css'
 import { BrowserRouter, Route, Routes } from 'react-router'
 import Home from './pages/Home'
-import WhatsAppAIAssistant from './pages/WhatsAppAIAssistant'
-import LamakiConstruction from './pages/LamakiConstruction'
-import EventsWebsite from './pages/EventsWebsite'
-import ConstructionSite from './pages/ConstructionSite'
-import AIProjects from './pages/AIProjects'
+
 
 function App() {
  
@@ -16,11 +12,7 @@ function App() {
     <BrowserRouter>
     <Routes>
       <Route path='/' element={<Home/>} />
-      <Route path="/projects/whatsapp-bot" element={<WhatsAppAIAssistant />} />
-      <Route path="/projects/lamaki" element={<LamakiConstruction />} />
-      <Route path="/projects/events" element={<EventsWebsite />} />
-      <Route path="/projects/construction" element={<ConstructionSite />} />
-      <Route path="/projects/ai-projects" element={<AIProjects />} />
+    
 
     </Routes>
     </BrowserRouter>
